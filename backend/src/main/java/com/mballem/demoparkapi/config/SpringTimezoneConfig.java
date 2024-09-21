742066ef219c30ed3790815c433d45efd66efe80
Dyego Lima <dyegodll18@gmail.com>
2024-09-21 17:27:36 -0300

    correção de erros de plugin no maven + arquivos do curso

--- backend/src/main/java/com/mballem/demoparkapi/config/SpringTimezoneConfig.java
+++ backend/src/main/java/com/mballem/demoparkapi/config/SpringTimezoneConfig.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
 
-//classe para configuração do TimeZone do país para data e hora correspondente
+//classe para configuração do TimeZone do país para data e hora correspondentes
 
 @Configuration
 public class SpringTimezoneConfig {
 
-	@PostConstruct //define a execução do método após a inicialização dos construtores do Spring
+	@PostConstruct //define a execução do método após a inicialização do construtor do Spring
 	public void timezoneConfig() {
 		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo")); //String-ID TimeZone do Brasil
 	}
